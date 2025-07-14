d3f7a46d763dacb29867b9215426b94f1e1826df
Infamous003 <arfathsyed0312@gmail.com>
2025-07-14 22:37:16 +0530

    Add some more styling

--- src/About.jsx
+++ src/About.jsx
@@ -6,14 +6,14 @@ export default function About() {
             <br />
             <p>I mostly use <b>Python</b> with <b>FastAPI</b> and <b>PostgreSQL</b> for back-end and <b>JavaScript</b> with <b>React</b> for the font-end.</p>
             <br />
-            <p>When I'm bored of submitting job applications, I spend time dockerizing my projects and learn Go. And when bored of that, I love playing anger inducing games like Sekiro and Dark Souls 3</p>
+            <p>When I'm bored of submitting job applications, I spend time dockerizing my projects and learn <b>Go</b>. And when bored of that, I love playing anger inducing games like <b>Sekiro</b> and <b>Dark Souls 3</b></p>
             <br />
 
             <p>You can find me on:</p>
             <ul className="socials-container">
                 <li>&gt;<a href="https://github.com/Infamous003" className="socials-link">GitHub</a></li>
-                <li>&gt;<a href="" className="socials-link">Twitter</a></li>
-                <li>&gt;<a href="" className="socials-link">LinkedIn</a></li>
+                <li>&gt;<a href="#" className="socials-link">Twitter</a></li>
+                <li>&gt;<a href="#" className="socials-link">LinkedIn</a></li>
             </ul>
             <br />
         </div>
