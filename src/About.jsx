cb5b19353e6b12bed0f6e6c8376a874c2d52522a
Infamous003 <arfathsyed0312@gmail.com>
2025-07-14 22:19:50 +0530

    Add consistant font sizing and shorten the about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,23 +1,21 @@
 export default function About() {
-
     return(
         <div className="about-section">
             <h1 className="greetings">Hey there! Syed here,</h1>
             <p className="about-subtitle"><b>I'm a recent graduate and a backend developer in the making.</b></p>
             <br />
-            <p>I mostly use <b>Python</b> with <b>FastAPI</b> and <b>PostgreSQL</b> for back-end and <b>JavaScript</b> with <b>React</b> for the font-end</p>
+            <p>I mostly use <b>Python</b> with <b>FastAPI</b> and <b>PostgreSQL</b> for back-end and <b>JavaScript</b> with <b>React</b> for the font-end.</p>
+            <br />
             <p>When I'm bored of submitting job applications, I spend time dockerizing my projects and learn Go. And when bored of that, I love playing anger inducing games like Sekiro and Dark Souls 3</p>
             <br />
 
             <p>You can find me on:</p>
             <ul className="socials-container">
-                <li>&gt; GitHub &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Infamous003">github.com/Infamous003</a></li>
-                <li>&gt; Twitter &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">x.com/Infamous0312</a></li>
-                <li>&gt; LinkedIn &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">linkedin.com/in/syedmehdi03/</a></li>
-                <li>&gt; Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="syedmehdi1233#gmail.com">syedmehdi1233@gmail.com</a></li>
+                <li>&gt;<a href="https://github.com/Infamous003" className="socials-link">GitHub</a></li>
+                <li>&gt;<a href="" className="socials-link">Twitter</a></li>
+                <li>&gt;<a href="" className="socials-link">LinkedIn</a></li>
             </ul>
             <br />
-            <p>I created my first email back in 6th grade and used 'infamous' as my username which I thought was pretty cool. Ever since then, I have stuck with it for almost everything!</p>
         </div>
     )
 }
